f23c4adf6f7e4cacff06f202010207ce564b0750
Maxi Marufo <31772558+maxi-marufo@users.noreply.github.com>
2019-08-11 18:25:49 -0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(input_array, value):
+    length = len(input_array)
+    mid = length/2
+    new_array = input_array
+    idx = 0
+    while (mid !=0):
+        if new_array[mid] < value:
+            idx += (mid+1)
+            new_array = new_array[(mid+1):]
+            mid = len(new_array)/2
+        else:
+            new_array = new_array[:mid]
+            mid = len(new_array)/2
+        if new_array[mid] == value:
+            return idx
+    return -1
